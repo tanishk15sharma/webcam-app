19e04fb676f01bcdfcfd606030ff2cba29e1b55d
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-02 23:19:26 +0530

    javaScript added

---
+++ app.js
@@ -0,0 +1,6 @@
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
+
