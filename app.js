e6d4d7cfa377b674e06841a377c7a462d9f25e4b
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-04 00:23:43 +0530

    filters and btn-style added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
 
+
+//this function will give turn the camera on and it will show the video on UI
 function getVideo() {
     navigator.mediaDevices.getUserMedia({video: true , audio: false })
     .then(localMediaStream => {
@@ -17,7 +19,7 @@ function getVideo() {
     } )
 }
 
-
+//this function print exact images in the videos and display it in full screen 
 function paintToCanavas() {
     const width = video.videoWidth;
     const height = video.videoHeight;
@@ -27,11 +29,16 @@ function paintToCanavas() {
 
 
    return setInterval( () =>{
-        ctx.drawImage(video,0,0,width,height  )
+        ctx.drawImage(video,0,0,width,height  );
+        let pixels = ctx.getImageData(0, 0, width, height);
 
+        pixels = rgbSplit(pixels);
+
+        ctx.putImageData(pixels, 0, 0);
     }  ,16)
 }
 
+//this function will take the photo/ss when clicked
 function takePhoto() {
     snap.currentTime = 0;
     snap.play();
@@ -45,9 +52,53 @@ function takePhoto() {
     // link.textContent = "download Image"; this will show a download image link ,user will click it and the ss will be downloaded here
     strip.insertBefore(link,strip.firstChild);
 
-
-
 }
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+      pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+      pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+      pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels;
+  }
+  
+  function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+      pixels.data[i - 150] = pixels.data[i + 0]; // RED
+      pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+      pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+    }
+    // return pixels;
+  }
+  
+  function greenScreen(pixels) {
+    const levels = {};
+  
+    document.querySelectorAll('.rgb input').forEach((input) => {
+      levels[input.name] = input.value;
+    });
+  
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+      red = pixels.data[i + 0];
+      green = pixels.data[i + 1];
+      blue = pixels.data[i + 2];
+      alpha = pixels.data[i + 3];
+  
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        // take it out!
+        pixels.data[i + 3] = 0;
+      }
+    }
+  
+    return pixels;
+  }
+
 
 video.addEventListener("canplay",paintToCanavas )
 
