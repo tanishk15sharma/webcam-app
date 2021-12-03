d12910c6ea1012ef2e2267d11c4dce350f3b26f3
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-03 21:48:04 +0530

    video camera enabled

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
 
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true , audio: false })
+    .then(localMediaStream => {
+        console.log(localMediaStream);
+        video.srcObject = localMediaStream;
+        // video.src = window.URL.createObjectURL(localMediaStream)
+        video.play()
+    } )
+    .catch(err =>{
+        console.error(`OH NOOO!`,err  );
+    } )
+}
+
+// console.log(getVideo());
+// getVideo()
