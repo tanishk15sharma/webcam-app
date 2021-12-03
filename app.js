6ce113457877b3776d7d57fb07b7ea849dc629a9
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-03 23:16:45 +0530

    photo downloader added

--- app.js
+++ app.js
@@ -17,5 +17,41 @@ function getVideo() {
     } )
 }
 
+
+function paintToCanavas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    // console.log(width , height);
+    canvas.width = width;
+    canvas.height = height;
+
+
+   return setInterval( () =>{
+        ctx.drawImage(video,0,0,width,height  )
+
+    }  ,16)
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL("image/jpeg");
+    // console.log(data);
+    const link = document.createElement("a");
+    link.href = data;
+    link.setAttribute("download","your screenshort");
+    // link.textContent = "download Image"; this will show a download image link ,user will click it and the ss will be downloaded here
+    strip.insertBefore(link,strip.firstChild);
+
+}
+
+video.addEventListener("canplay",paintToCanavas )
+
+//  paintToCanavas()
 // console.log(getVideo());
-// getVideo()
+getVideo()
+
+
+
+
